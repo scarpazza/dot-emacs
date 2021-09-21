19e823632c709eb3aa337c655c357ad066705971
Daniele Paolo Scarpazza <scarpaz@DanielesMBPAir.scarpaz>
2021-09-21 11:17:25 -0400

    Pick up jira-md-mode from main elisp file

--- .emacs.d/scarpaz-main.el
+++ .emacs.d/scarpaz-main.el
@@ -2,6 +2,7 @@
 (load "~/.emacs.d/scarpaz-calendar.el")
 (load "~/.emacs.d/scarpaz-spell-check.el")
 (load "~/.emacs.d/scarpaz-user-lookup.el")
+(load "~/.emacs.d/scarpaz-jira-md-mode.el")
 (load "~/.emacs.d/scarpaz-key-bindings.el")
 (load "~/.emacs.d/scarpaz-battlecruiser.el")
 (load "~/.emacs.d/scarpaz-save-hooks.el")
