0ba063dfc6a83356aa15cb1b7c88258c7b2317d3
Daniele Paolo Scarpazza <scarpaz@DanielesMBPAir.scarpaz>
2021-09-21 22:26:55 -0400

    jira-md minor mode now resolves user mentions more efficiently than old user lookup file

--- .emacs.d/scarpaz-jira-md-mode.el
+++ .emacs.d/scarpaz-jira-md-mode.el
@@ -1,27 +1,31 @@
 ;;
 ;; jira-md mode
 ;;
+;; scarpaz@scarpaz.com
+;;
+;; All the files in these repository are released under the GNU General Public License v3.
+;; To learn more, see https://www.gnu.org/licenses/gpl-3.0.en.html
+;;
 ;; This is a minor mode that performs the following:
 ;; - it colorizes user mentions and jira issue numbers
+;; - it resolves user mentions via "finger" when you type or move your cursor on them;
+;;   It uses a hash table to resolve usernames (and substrings) only once.
 ;; - it binds "Alt-Enter" to issue lookup via org-jira.
 ;;
 ;; It's intended to be consistent with markdown rendering performed by Atlassian products
 ;; like bitbucket.
 ;;
-;; It auto-enables when editing markdown.
+;; It auto-enables when editing markdown files.
 ;;
 
 
 (require 'font-lock)
 (require 'org-jira)
 
-
-
 (setq jira-md-keywords
       '(("[A-Z]+-[0-9]+" . font-lock-function-name-face)
         ("@[a-z0-9_]+"    . font-lock-constant-face))
       )
-
 
 ;;;###autoload
 (define-minor-mode jira-md-mode
@@ -38,8 +42,13 @@
     (when font-lock-mode
       (with-no-warnings (font-lock-fontify-buffer)))
     )
-)
 
+  (setq scarpaz/user-hash-table (make-hash-table :test 'equal))
+  (add-hook 'post-command-hook 'scarpaz/cursor-hook)
+  )
+
+
+;; rewrite me with thing-at-point 'symbole
 (defun scarpaz/go-jira ()
   (interactive)
   (let* (
@@ -51,19 +60,42 @@
          (issue_no  (buffer-substring-no-properties start end))
          )
     (goto-char initialpt)
-    (message "Requesting jira issue %s" issue_no)
     (org-jira-get-issue issue_no)
     )
- )
+  )
 
+(defun scarpaz/display-user-mention (username beg end)
+  (setq hashlookup (gethash username scarpaz/user-hash-table))
+  (if hashlookup
+      (message hashlookup)
+    (progn
+      (message "Resolving user mention: %s ..." username )
+      (puthash username (shell-command-to-string (format "finger -m %s" username))
+               scarpaz/user-hash-table)
+      (message "+%s" (gethash username scarpaz/user-hash-table))))
+)
+
+
+(defun scarpaz/cursor-hook ()
+  (interactive)
+  (let* (
+         ( bounds (bounds-of-thing-at-point 'symbol) )
+         ( beg    (car bounds))
+         ( end    (cdr bounds))
+         )
+    (progn
+      (when beg
+        (when (char-before beg)
+          (when (char-equal ?@ (char-before beg))
+            (setq str (buffer-substring-no-properties beg end))
+            (scarpaz/display-user-mention str beg end)
+            ))))
+))
+
+(add-hook 'post-command-hook 'scarpaz/cursor-hook)
 
 ;;;###autoload
 (add-hook 'markdown-mode-hook 'jira-md-mode)
 
 (provide 'jira-md-mode)
-
-;; HPC-123
-
-
-
 
--- .emacs.d/scarpaz-main.el
+++ .emacs.d/scarpaz-main.el
@@ -1,7 +1,6 @@
 (load "~/.emacs.d/scarpaz-edit-prefs.el")
 (load "~/.emacs.d/scarpaz-calendar.el")
 (load "~/.emacs.d/scarpaz-spell-check.el")
-(load "~/.emacs.d/scarpaz-user-lookup.el")
 (load "~/.emacs.d/scarpaz-jira-md-mode.el")
 (load "~/.emacs.d/scarpaz-key-bindings.el")
 (load "~/.emacs.d/scarpaz-battlecruiser.el")
